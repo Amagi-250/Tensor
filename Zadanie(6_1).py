26cff0861115dd24a48844f9895738d43e0138aa
Amagi-250 <94859678+Amagi-250@users.noreply.github.com>
2021-12-04 16:42:28 +0300

    Add files via upload

--- Zadanie(6_1).py
+++ Zadanie(6_1).py
@@ -33,8 +33,8 @@ print('\n---------')
 print('Задание 2')
 print('---------')
 
-input = open('Input.txt', 'r')
-output = open('Output.txt', 'w')
+input = open('Input2', 'r')
+output = open('Output2', 'w')
 
 try:
     '''Обращение к первому элементу в файле, взятие значения для молекулы C'''
@@ -61,43 +61,4 @@ except RuntimeError:
 finally:
     input.close()
     output.close()
-    print('Файл Output.txt был изменён')
-
-print('\n---------')
-print('Задание 3')
-print('---------')
-
-def xor_cipher(strg, key):
-    encript_str = ""
-    for letter in strg:
-        for keyy in key:
-            encript_str += chr(ord(letter) ^ ord(keyy))
-            break
-    return encript_str
-
-inp = open('Input1.txt', 'r', encoding='utf-8')
-out = open('Output1.txt', 'w')
-
-try:
-    strg = inp.readline()
-    key = input('Введите ключ: ')
-
-    encr_strg = xor_cipher(strg, key)
-
-    out.write(f'Original: {strg}')
-    out.write(f'\nEncode: {encr_strg}')
-    out.write(f'\nDecode: {xor_cipher(encr_strg, key)}')
-
-except ArithmeticError:
-    print('Арифметическая ошибка')
-except EOFError:
-    print('Функция наткнулась на конец файла и не смогла прочитать то, что хотела')
-except SyntaxError:
-    print('Синтаксическая ошибка')
-except RuntimeError:
-    print('Неизвестная ошибка')
-
-finally:
-    inp.close()
-    out.close()
-    print('Файл Output.txt был изменён')
+    print('Файл Output2.txt был изменён')
