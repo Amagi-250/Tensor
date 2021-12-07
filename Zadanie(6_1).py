25fd43db3be3a9c1f24859f97fef1ddb6f743f2f
Amagi-250 <94859678+Amagi-250@users.noreply.github.com>
2021-12-07 11:56:04 +0300

    Update Zadanie(6_1).py

--- Zadanie(6_1).py
+++ Zadanie(6_1).py
@@ -6,28 +6,31 @@ str1 = 'Привет'
 str2 = 'это'
 str3 = 'программа'
 
+""" str_ - список строк, объединённых в одну """
 try:
     str_ = str1 + ', ' + str2 + ', ' + str3
-
-    def encode(str):
-        return str.encode('utf-8')
-
-    def decode(str):
-        return str.decode('utf-8')
-
-    enc = encode(str_)
-    print('Список байт кодов: ', enc)
-
-    dec = decode(enc)
-    print('Список строк: ', dec)
-
 except Exception:
     print('Ошибка преобразования')
 except ArithmeticError:
     print('Арифметическая ошибка')
 
-finally:
-    pass
+
+# Функция преобразования список строк в список байт-кодов      
+def encode(str):
+    return str.encode('utf-8')
+
+
+# Функция преобразования списка байт-кодов в список строк 
+def decode(str):
+    return str.decode('utf-8')
+
+
+enc = encode(str_)
+print('Список байт кодов: ', enc)
+
+dec = decode(enc)
+print('Список строк: ', dec)
+
 
 print('\n---------')
 print('Задание 2')
@@ -37,27 +40,39 @@ input = open('Input2', 'r')
 output = open('Output2', 'w')
 
 try:
-    '''Обращение к первому элементу в файле, взятие значения для молекулы C'''
+    ''' Обращение к первому элементу в файле, взятие значения для молекулы C '''
     C = int(input.read(1))
-    '''Обращение к второму элементу в файле, взятие значения молекулы H'''
-    H = int(input.read(2))
-    '''Обращение к третьему элементу в файле, взятие значения молекулы O'''
-    O = int(input.read(3))
-    '''Расчёт числа молекул в формуле спирта'''
-    rezult = C*2+H*6+O
-    output.write(f'The maximum possible number of alcohol molecules: {str(rezult)}')
-
-except ZeroDivisionError:
-    print('Была произведена попытка деления на ноль')
 except Exception:
     print('Ошибка взятия чисел из файла')
 except EOFError:
     print('Функция наткнулась на конец файла и не смогла прочитать то, что хотела')
-except SyntaxError:
-    print('Синтаксическая ошибка')
-except RuntimeError:
-    print('Неизвестная ошибка')
 
+try:
+    '''Обращение к второму элементу в файле, взятие значения молекулы H'''
+    H = int(input.read(2))
+except Exception:
+    print('Ошибка взятия чисел из файла')
+except EOFError:
+    print('Функция наткнулась на конец файла и не смогла прочитать то, что хотела')
+    
+try:
+    '''Обращение к третьему элементу в файле, взятие значения молекулы O'''
+    O = int(input.read(3))
+except Exception:
+    print('Ошибка взятия чисел из файла')
+except EOFError:
+    print('Функция наткнулась на конец файла и не смогла прочитать то, что хотела')
+    
+try:
+    '''Расчёт числа молекул в формуле спирта'''
+    rezult = C*2 + H*6 + O
+except ZeroDivisionError:
+    print('Была произведена попытка деления на ноль')
+
+try:
+    output.write(f'The maximum possible number of alcohol molecules: {str(rezult)}')
+except Exception:
+    print('Ошибка записи результата в файл')
 finally:
     input.close()
     output.close()
