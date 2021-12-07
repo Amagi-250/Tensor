087f355d174d06b1f70dcdcfb64c04c05edcec02
Amagi-250 <94859678+Amagi-250@users.noreply.github.com>
2021-12-07 12:07:19 +0300

    Update Zadanie(6_2).py

--- Zadanie(6_2).py
+++ Zadanie(6_2).py
@@ -1,8 +1,11 @@
-
-print('\n---------')
+print('---------')
 print('Задание 3')
 print('---------')
 
+
+# Функция шифрования по методу XOR
+""" letter - сообщение для шифрования 
+    keyy - ключ для шифрования """
 def xor_cipher(strg, key):
     encript_str = ""
     for letter in strg:
@@ -11,28 +14,42 @@ def xor_cipher(strg, key):
             break
     return encript_str
 
-inp = open('Input1.txt', 'r', encoding='utf-8')
-out = open('Output1.txt', 'w')
 
 try:
-    strg = inp.readline()
-    key = input('Введите ключ: ')
+    inp = open('Input1.txt', 'r', encoding='utf-8')
+except RuntimeError:
+    print('Ошибка чтения файла Input1.txt')
 
-    encr_strg = xor_cipher(strg, key)
+try:
+    out = open('Output1.txt', 'w')
+except RuntimeError:
+    print('Ошибка чтения файла Output1.txt')
+    
+strg = inp.readline()
+key = input('Введите ключ: ')
 
+encr_strg = xor_cipher(strg, key)
+
+try:
     out.write(f'Original: {strg}')
-    out.write(f'\nEncode: {encr_strg}')
-    out.write(f'\nDecode: {xor_cipher(encr_strg, key)}')
-
-except ArithmeticError:
-    print('Арифметическая ошибка')
-except EOFError:
-    print('Функция наткнулась на конец файла и не смогла прочитать то, что хотела')
 except SyntaxError:
     print('Синтаксическая ошибка')
 except RuntimeError:
-    print('Неизвестная ошибка')
+    print('Ошибка записи Original в файл')
 
+try:
+    out.write(f'\nEncode: {encr_strg}')
+except SyntaxError:
+    print('Синтаксическая ошибка')
+except RuntimeError:
+    print('Ошибка записи Encode в файл')
+
+try: 
+    out.write(f'\nDecode: {xor_cipher(encr_strg, key)}')
+except SyntaxError:
+    print('Синтаксическая ошибка')
+except RuntimeError:
+    print('Ошибка записи Decode в файл')
 finally:
     inp.close()
     out.close()
