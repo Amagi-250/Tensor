fe98a196a813920522514e575e60a848cbf15386
Amagi-250 <94859678+Amagi-250@users.noreply.github.com>
2021-12-07 11:45:56 +0300

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -18,33 +18,36 @@ finally:
     pass
 
 
-#Функция проверки пароля на количество символов большее или равно 6
+""" Функция проверки пароля на количество символов большее или равно 6 """
 def symbols(password):
     if len(password) >= 6:
         return True
 
 
-#Функция проверки на то, что пароль содержит хотя бы одну цифру
+""" Функция проверки на то, что пароль содержит хотя бы одну цифру """
 def num(password):
     if re.search(r'\d', password):
         return True
 
 
-#Функция проверки на то, что пароль содержит символы и верхнего и нижного регистров
+""" Функция проверки на то, что пароль содержит символы и верхнего и нижного регистров """
 def reg(password):
     if not (password.upper() == password or password.lower() == password):
         return True
 
 
-#Функция проверки на то, что пароль не состоит только из цифр
+"""" Функция проверки на то, что пароль не состоит только из цифр """ 
 def numbers(password):
     if not password.isdecimal():
         return True
 
 
-#Функция окончательной проверки на соответствия пароля всем условиям
+""" Функция окончательной проверки на соответствия пароля всем условиям """
 def pwd(password):
-    if symbols(password) and num(password) and reg(password) and numbers(password) == True:
+    if symbols(password) 
+        and num(password) 
+        and reg(password) 
+        and numbers(password):
         print('--------------------------------------------')
         print('Пароль удовлетворяет всем условиям проверки!')
     else:
@@ -54,11 +57,11 @@ def pwd(password):
 
 pwd(password)
 
-print('---------')
+print('\n---------')
 print('Задание 2')
 print('---------')
 
-
+""" Функция для принятия любого количества переменных"""
 def znach(*num):
 
     sum = 0
@@ -72,36 +75,19 @@ znach(1, 2, 3)
 znach(1, 2, 3, 4)
 znach(1, 2, 3, 4, 5)
 
-print('---------')
+print('\n---------')
 print('Задание 3')
 print('---------')
 
-fib1 = fib2 = 1
 
-try:
-    n = int(input("Задайте колличество чисел в ряду Фибоначчи: "))
-except NameError:
-    print("Не найдено переменной с таким именем")
-except TypeError:
-    print("Применение к объекту несоответствующего типа")
-except RuntimeError:
-    print("Исключение")
-finally:
-    pass
+# Рекурсивная функция возвращающая заданное число Фибоначчи
+def fibonacci(n):
+    """ Числа 1 и 2, это соответственно первый 
+        и второй элементы ряда Фибоначчи """
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
-print("Ряд Фибоначчи:")
-print(fib1, fib2, end=' ')
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end=' ')
 
-fib1 = fib2 = 1
-
-n = int(input("\nВведите номер элемента ряда Фибоначчи: "))
-n = int(n) - 2
-
-while n > 0:
-    fib1, fib2 = fib2, fib1 + fib2
-    n -= 1
-
-print("Значение этого элемента:", fib2)
+el = int(input("Введите индекс элемента ряда Фибоначчи: "))
+print(f"Число Фибоначчи под индексом {el} равно: {fibonacci(el)}")
