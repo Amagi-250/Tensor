486f1737921e79eec812ef7e8fe7ba8e068ec932
Amagi-250 <94859678+Amagi-250@users.noreply.github.com>
2021-12-18 10:36:28 +0300

    Update test_circl.py

--- test_circl.py
+++ test_circl.py
@@ -1,9 +1,14 @@
 import circle
 
+
 class TestCircle:
 
+    
+    #Первая часть разработки через тестирование TDD
     def test_square_5(self):
         assert circle.square(5) == 78.53981633974483
-
+    
+    
+    #Вторая часть (новое условие задачи) разработки через тестирование TDD
     def test_perimeter_5(self):
         assert circle.perimeter(5) == 31.41592653589793
